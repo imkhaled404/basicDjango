77fe325d1e34382c84f5666051ed6e35a00bb428
Shezan Mahmud <shezan@constantmd.com>
2021-01-26 16:37:56 +0600

    Markdown Post

---
+++ backend/api/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Post 
+
+from django.contrib import admin
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status','created_on')
+    list_filter = ("status", "created_on")
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    
+
+admin.site.register(Post, PostAdmin)
---
+++ backend/api/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import generic
+from .models import Post
+
+
+def index(request):
+
+    posts = Post.objects.all() 
+    context = {
+        'posts': posts.order_by('-created_on')
+
+    }
+
+    return render(request, 'index.html', context)
